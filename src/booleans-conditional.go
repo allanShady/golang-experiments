f7321e62973538394ac77b940e2c58a7c2f80f37
allanShady <acamilo.ag@gmail.com>
2021-08-23 21:47:08 +0200

    chore: add continue and break usage

--- src/booleans-conditional.go
+++ src/booleans-conditional.go
@@ -12,4 +12,16 @@ func main() {
 	} else {
 		fmt.Println("Not found")
 	}
+
+	//continue and break
+	players := []string{"Messi", "Cristiano", "m'bape", "Neymar", "Tester"}
+
+	for position, player := range players {
+		if position == 1 {
+			fmt.Printf("continue at position: %v and the value was: %s\n", position, player)
+		} else if position > 2 {
+			fmt.Printf("break at position %v\n", position)
+			break
+		}
+	}
 }
