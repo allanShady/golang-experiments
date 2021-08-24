07f33a3478b9d5c89482af3fe94dc65ea01629ef
allanShady <acamilo.ag@gmail.com>
2021-08-24 20:06:58 +0200

    chore: add function that return a value

--- src/functions.go
+++ src/functions.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Greeting(name string) {
 	fmt.Println("Hello, ", name)
@@ -11,9 +14,16 @@ func GreetingPlayers(players []string, f func(string)) {
 		f(player)
 	}
 }
+
+func CircleArea(radius float64) float64 {
+	return math.Pi * radius * radius
+}
+
 func main() {
 
 	Greeting("Allan")
-	GreetingPlayers([]string {"Ronaldo", "Messi", "Nani", "Levandosk"}, Greeting)
+	GreetingPlayers([]string{"Ronaldo", "Messi", "Nani", "Levandosk"}, Greeting)
 
+	circle_area := CircleArea(9)
+	fmt.Printf("The final area is: %0.2f\n", circle_area)
 }
