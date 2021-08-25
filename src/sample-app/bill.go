c59accbc8c2afd0ecbfae6066df60fe8a7e38d8f
allanShady <acamilo.ag@gmail.com>
2021-08-25 06:04:20 +0200

    chore: add receiver function use case

--- src/sample-app/bill.go
+++ src/sample-app/bill.go
@@ -1,15 +1,31 @@
 package main
 
-type bill struct {
+import "fmt"
+
+type Bill struct {
 	name  string
 	items map[string]float64
 	tip   float64
 }
 
-func FactoryBill(name string) bill {
-	return bill{
+func FactoryBill(name string) Bill {
+	return Bill{
 		name:  name,
-		items: map[string]float64{},
+		items: map[string]float64{"coffee": 5.087, "xima": 21.879},
 		tip:   0,
 	}
+}
+
+func (bill Bill) FormatOutput() string {
+	output := "Bills break down: \n"
+	var total_bills float64 = 0
+
+	for key, value := range bill.items {
+		output += fmt.Sprintf("%-25v ...$%0.2f\n", key+":", value)
+		total_bills += value
+	}
+
+	output += fmt.Sprintf("%-25v ...$%0.2f\n", "total:", total_bills)
+
+	return output
 }
